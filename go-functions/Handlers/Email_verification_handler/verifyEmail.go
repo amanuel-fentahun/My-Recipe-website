c973906b30f198bad68346221f8eff45f2f910db
Amanuel Fentahun <6186fre@gmail.com>
2025-09-19 01:54:31 +0300

    finishing up the verifyEmail by adding a way to activate the user after successful email verification

--- go-functions/Handlers/Email_verification_handler/verifyEmail.go
+++ go-functions/Handlers/Email_verification_handler/verifyEmail.go
@@ -57,7 +57,40 @@ func VerifyEmailHandler(c *gin.Context) {
 
 	if valid {
 		// update the user table isVerified to true
-		c.JSON(http.StatusOK, gin.H{"message": "Your email is verifed successfully. Please login to your account to see your profile! Thank you."})
+		var Mutation struct {
+			UpdateUsers struct {
+				Affected_rows int `graphql:"affected_rows"`
+			} `graphql:"update_Users(where: {email: {_eq: $email}}, _set: {isVerified: true})"`
+		}
+
+		// delete the verificationData after succussfully verify the email
+		var Mutation2 struct {
+			DeleteVerificationData struct {
+				Email string `graphql:"email"`
+			} `graphql:"delete_VerificationData_by_pk(email: $email)"`
+		}
+
+		vars2 := map[string]interface{}{
+			"email": graphql.String(email),
+		}
+
+		if err := client.Mutate(context.Background(), &Mutation, vars2); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "something went wrong. Please try again later!"})
+			return
+		}
+
+		// if isVerified updated successfully delete the verificationData
+		if Mutation.UpdateUsers.Affected_rows == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to verify your email. Please try again later!"})
+			return
+		}
+
+		if err := client.Mutate(context.Background(), &Mutation2, vars2); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "something went wrong. Please try again later!"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Your email is verifed successfully. Please login to your account to access your profile! Thank you."})
 		return
 	}
 }
